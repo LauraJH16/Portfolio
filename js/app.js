06f60ec03a17fe8c93f77bfa89bb2729d67f065c
LauraJH16 <laurajimenezh16@hotmail.com>
2018-02-23 17:30:38 -0500

    Portafolio v2.0

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-window.addEventListener('load',()=>{
-    var ancor = document.getElementsByClassName('nav-link');
-    if(ancor.className = 'active'){
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.nav-wrapper').addClass('scroll-down')
+      $('.logo').attr('src' ,'assets/images/logo2.png');
+      $('.logo').css('width' ,'90px');
+      $('header ul li a').css('color','black');
+      $('.main-menu').css('margin-top','10px');
+    } else {
+      $('.nav-wrapper').removeClass('scroll-down');
+      $('.logo').attr('src' ,'assets/images/logo.png');
+      $('header ul li a').css('color','white');
+      $('.logo').css('width' ,'112px');
+      $('.main-menu').css('margin-top','20px');
     }
-})
+  });
+
+ 
