0fc810d2910db7bfdeec0988ea58b2ca17c1ae6f
LauraJH16 <laurajimenezh16@hotmail.com>
2018-02-24 02:07:48 -0500

    Portafolio v2.0

--- js/app.js
+++ js/app.js
@@ -1,17 +1,18 @@
-$(window).scroll(function () {
+$(document).ready(function () {
+
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('.nav-wrapper').addClass('scroll-down')
-      $('.logo').attr('src' ,'assets/images/logo2.png');
-      $('.logo').css('width' ,'90px');
-      $('header ul li a').css('color','black');
-      $('.main-menu').css('margin-top','10px');
+      $('.logo').attr('src', 'assets/images/logo2.png');
+      $('.logo').css('width', '90px');
+      $('header ul li a').css('color', 'black');
+      $('.main-menu').css('margin-top', '10px');
     } else {
       $('.nav-wrapper').removeClass('scroll-down');
-      $('.logo').attr('src' ,'assets/images/logo.png');
-      $('header ul li a').css('color','white');
-      $('.logo').css('width' ,'112px');
-      $('.main-menu').css('margin-top','20px');
+      $('.logo').attr('src', 'assets/images/logo.png');
+      $('header ul li a').css('color', 'white');
+      $('.logo').css('width', '112px');
+      $('.main-menu').css('margin-top', '20px');
     }
   });
-
- 
+});
