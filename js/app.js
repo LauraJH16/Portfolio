dfadea7156640da6fb617837fd784f135f53ac96
LauraJH16 <laurajimenezh16@hotmail.com>
2018-03-26 09:47:25 -0500

    Mobile version

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
   $(window).scroll(function () {
@@ -5,14 +6,28 @@ $(document).ready(function () {
       $('.nav-wrapper').addClass('scroll-down')
       $('.logo').attr('src', 'assets/images/logo2.png');
       $('.logo').css('width', '90px');
+      $('.button-collapse').css('color','black');
       $('header ul li a').css('color', 'black');
       $('.main-menu').css('margin-top', '10px');
     } else {
       $('.nav-wrapper').removeClass('scroll-down');
       $('.logo').attr('src', 'assets/images/logo.png');
+      $('.button-collapse').css('color','white');
       $('header ul li a').css('color', 'white');
       $('.logo').css('width', '112px');
       $('.main-menu').css('margin-top', '20px');
     }
   });
+
+
+  $('.button-collapse').sideNav({
+      menuWidth: 200, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen:   $('.button-collapse').sideNav('show') , // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff*/ }, // A function to be called when sideNav is closed
+    }
+  );
+
 });
